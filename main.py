6958d7efd23082d24c74fa278e11ba6aeec1f92e
John M <github.fox-io@foxdata.io>
2023-03-14 06:44:03 -0500

    Clean up button code.
    Use loops for making buttons

--- main.py
+++ main.py
@@ -11,43 +11,35 @@ display_font = ("Consolas", 20)
 display = tk.Label(root, text="0123456789", font=display_font, anchor="e")
 display.grid(row=0, column=0, columnspan=4, sticky="nesw")
 
+# Add buttons
+buttons = []
+function_text = ["C", "/", "*", "-", "+", "=", "."]
+
 # Add function buttons
-button_clear = tk.Button(root, text="C")
-button_clear.grid(row=1, column=0, sticky="nesw")
-button_divide = tk.Button(root, text="/")
-button_divide.grid(row=1, column=1, sticky="nesw")
-button_multiply = tk.Button(root, text="*")
-button_multiply.grid(row=1, column=2, sticky="nesw")
-button_subtract = tk.Button(root, text="-")
-button_subtract.grid(row=1, column=3, sticky="nesw")
-button_add = tk.Button(root, text="+")
-button_add.grid(row=2, column=3, rowspan=2, sticky="nesw")
-button_equals = tk.Button(root, text="=")
-button_equals.grid(row=4, column=3, rowspan=2, sticky="nesw")
-button_decimal = tk.Button(root, text=".")
-button_decimal.grid(row=5, column=2, sticky="nesw")
+for i in range(7):
+    button = tk.Button(root, text=function_text[i])
+    button.grid(row=i < 4 and 1 or i == 4 and 2 or i - 1,
+                column=i == 6 and 2 or 6 > i > 3 and 3 or i,
+                rowspan=3 < i < 6 and 2 or 1,
+                sticky="nesw")
+    buttons.append(button)
+    # c/*-+=. sym
+    # 1111245 rows
+    # 0123332 cols
+    # 0123456 i
 
 # Add number buttons
-button_0 = tk.Button(root, text="0")
-button_0.grid(row=5, column=0, columnspan=2, sticky="nesw")
-button_1 = tk.Button(root, text="1")
-button_1.grid(row=4, column=0, sticky="nesw")
-button_2 = tk.Button(root, text="2")
-button_2.grid(row=4, column=1, sticky="nesw")
-button_3 = tk.Button(root, text="3")
-button_3.grid(row=4, column=2, sticky="nesw")
-button_4 = tk.Button(root, text="4")
-button_4.grid(row=3, column=0, sticky="nesw")
-button_5 = tk.Button(root, text="5")
-button_5.grid(row=3, column=1, sticky="nesw")
-button_6 = tk.Button(root, text="6")
-button_6.grid(row=3, column=2, sticky="nesw")
-button_7 = tk.Button(root, text="7")
-button_7.grid(row=2, column=0, sticky="nesw")
-button_8 = tk.Button(root, text="8")
-button_8.grid(row=2, column=1, sticky="nesw")
-button_9 = tk.Button(root, text="9")
-button_9.grid(row=2, column=2, sticky="nesw")
+for i in range(10):
+    button = tk.Button(root, text=f"{i}")
+    if i == 0:
+        button.grid(row=5, column=0, columnspan=2, sticky="nesw")
+    if 1 <= i <= 3:
+        button.grid(row=4, column=i-1, sticky="nesw")
+    if 4 <= i <= 6:
+        button.grid(row=3, column=i-4, sticky="nesw")
+    if 7 <= i <= 9:
+        button.grid(row=2, column=i-7, sticky="nesw")
+    buttons.append(button)
 
 # Fill window with buttons
 root.columnconfigure("all", weight=1)
