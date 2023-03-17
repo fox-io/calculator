ae94e98d1df115d93da0cd5cac15aaf4b5e97d9e
John M <github.fox-io@foxdata.io>
2023-03-17 00:14:40 -0500

    Update main.py
    pygame UI

--- main.py
+++ main.py
@@ -1,120 +1,194 @@
-import tkinter as tk
+import pygame
 
-# Create and config the window
-root = tk.Tk()
-root.title("Calculator")
-root.geometry("200x300")
-root.resizable(False, False)  # Do not allow resize
+# Initialize Pygame
+pygame.init()
 
-# Calculation Variables
-left_value = "0"
-right_value = "0"
-current_function = ""
-current_result = "0"
+# Make Window
+window_size = (300, 475)
+screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption("Calculator")
 
-# Add the display
-display_font = ("Consolas", 20)
-display = tk.Label(root, text="0123456789", font=display_font, anchor="e")
-display.grid(row=0, column=0, columnspan=4, sticky="nesw")
+# Define Colors
+black = (0, 0, 0)
+grey = (128, 128, 128)
+light_grey = (192, 192, 192)
+white = (255, 255, 255)
 
+# Define Fonts
+font = pygame.font.SysFont("Consolas", 20)
 
-# Process keypress
-def keypress(key):
+# Define UI Components
+display = pygame.Rect(0, 0, 300, 100)
+button_c = pygame.Rect(0, 100, 75, 75)
+button_div = pygame.Rect(75, 100, 75, 75)
+button_mul = pygame.Rect(150, 100, 75, 75)
+button_sub = pygame.Rect(225, 100, 75, 75)
+button_add = pygame.Rect(225, 175, 75, 150)
+button_eq = pygame.Rect(225, 325, 75, 150)
+button_0 = pygame.Rect(0, 400, 150, 75)
+button_1 = pygame.Rect(0, 325, 75, 75)
+button_2 = pygame.Rect(75, 325, 75, 75)
+button_3 = pygame.Rect(150, 325, 75, 75)
+button_4 = pygame.Rect(0, 250, 75, 75)
+button_5 = pygame.Rect(75, 250, 75, 75)
+button_6 = pygame.Rect(150, 250, 75, 75)
+button_7 = pygame.Rect(0, 175, 75, 75)
+button_8 = pygame.Rect(75, 175, 75, 75)
+button_9 = pygame.Rect(150, 175, 75, 75)
+button_dot = pygame.Rect(150, 400, 75, 75)
 
-    global current_result, right_value, left_value, current_function
-    if key == "C":
-        clear()
-    elif key == "/" or key == "-" or key == "+" or key == "*":
-        current_function = key
-        left_value = right_value
-        right_value = "0"
-    elif key == "=":
-        if current_function == "/":
-            current_result = str(int(left_value) / int(right_value))
-        elif current_function == "*":
-            current_result = str(int(left_value) * int(right_value))
-        elif current_function == "-":
-            current_result = str(int(left_value) - int(right_value))
-        elif current_function == "+":
-            current_result = str(int(left_value) + int(right_value))
-        right_value = current_result
-    else:
-        if right_value == "0":
-            right_value = key
-        else:
-            right_value = str(right_value) + key
+running = True
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if button_c.collidepoint(event.pos):
+                print("C")
+            elif button_div.collidepoint(event.pos):
+                print("/")
+            elif button_mul.collidepoint(event.pos):
+                print("*")
+            elif button_sub.collidepoint(event.pos):
+                print("-")
+            elif button_add.collidepoint(event.pos):
+                print("+")
+            elif button_eq.collidepoint(event.pos):
+                print("=")
+            elif button_0.collidepoint(event.pos):
+                print("0")
+            elif button_1.collidepoint(event.pos):
+                print("1")
+            elif button_2.collidepoint(event.pos):
+                print("2")
+            elif button_3.collidepoint(event.pos):
+                print("3")
+            elif button_4.collidepoint(event.pos):
+                print("4")
+            elif button_5.collidepoint(event.pos):
+                print("5")
+            elif button_6.collidepoint(event.pos):
+                print("6")
+            elif button_7.collidepoint(event.pos):
+                print("7")
+            elif button_8.collidepoint(event.pos):
+                print("8")
+            elif button_9.collidepoint(event.pos):
+                print("9")
+            elif button_dot.collidepoint(event.pos):
+                print(".")
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                print("C")
+            elif event.key == pygame.K_KP_DIVIDE or event.key == pygame.K_SLASH:
+                print("/")
+            elif event.key == pygame.K_KP_MULTIPLY or event.key == pygame.K_ASTERISK:
+                print("*")
+            elif event.key == pygame.K_KP_MINUS or event.key == pygame.K_MINUS:
+                print("-")
+            elif event.key == pygame.K_KP_PLUS or event.key == pygame.K_PLUS:
+                print("+")
+            elif event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN:
+                print("=")
+            elif event.key == pygame.K_KP0 or event.key == pygame.K_0:
+                print("0")
+            elif event.key == pygame.K_KP1 or event.key == pygame.K_1:
+                print("1")
+            elif event.key == pygame.K_KP2 or event.key == pygame.K_2:
+                print("2")
+            elif event.key == pygame.K_KP3 or event.key == pygame.K_3:
+                print("3")
+            elif event.key == pygame.K_KP4 or event.key == pygame.K_4:
+                print("4")
+            elif event.key == pygame.K_KP5 or event.key == pygame.K_5:
+                print("5")
+            elif event.key == pygame.K_KP6 or event.key == pygame.K_6:
+                print("6")
+            elif event.key == pygame.K_KP7 or event.key == pygame.K_7:
+                print("7")
+            elif event.key == pygame.K_KP8 or event.key == pygame.K_8:
+                print("8")
+            elif event.key == pygame.K_KP9 or event.key == pygame.K_9:
+                print("9")
+            elif event.key == pygame.K_KP_PERIOD or event.key == pygame.K_PERIOD:
+                print(".")
 
-    update_display()
+    screen.fill(white)
 
-    # Return break to prevent duplicated input
-    return "break"
+    pygame.draw.rect(screen, white, display)
+    pygame.draw.rect(screen, grey, display, 1)
+    display_text = font.render("0", True, black)
+    screen.blit(display_text, display_text.get_rect(right=display.right - 10, centery=display.centery))
+    pygame.draw.rect(screen, light_grey, button_c)
+    pygame.draw.rect(screen, grey, button_c, 1)
+    button_c_text = font.render("C", True, black)
+    screen.blit(button_c_text, button_c_text.get_rect(center=button_c.center))
+    pygame.draw.rect(screen, light_grey, button_div)
+    pygame.draw.rect(screen, grey, button_div, 1)
+    button_div_text = font.render("/", True, black)
+    screen.blit(button_div_text, button_div_text.get_rect(center=button_div.center))
+    pygame.draw.rect(screen, light_grey, button_mul)
+    pygame.draw.rect(screen, grey, button_mul, 1)
+    button_mul_text = font.render("*", True, black)
+    screen.blit(button_mul_text, button_mul_text.get_rect(center=button_mul.center))
+    pygame.draw.rect(screen, light_grey, button_sub)
+    pygame.draw.rect(screen, grey, button_sub, 1)
+    button_sub_text = font.render("-", True, black)
+    screen.blit(button_sub_text, button_sub_text.get_rect(center=button_sub.center))
+    pygame.draw.rect(screen, light_grey, button_add)
+    pygame.draw.rect(screen, grey, button_add, 1)
+    button_add_text = font.render("+", True, black)
+    screen.blit(button_add_text, button_add_text.get_rect(center=button_add.center))
+    pygame.draw.rect(screen, light_grey, button_eq)
+    pygame.draw.rect(screen, grey, button_eq, 1)
+    button_eq_text = font.render("=", True, black)
+    screen.blit(button_eq_text, button_eq_text.get_rect(center=button_eq.center))
+    pygame.draw.rect(screen, light_grey, button_0)
+    pygame.draw.rect(screen, grey, button_0, 1)
+    button_0_text = font.render("0", True, black)
+    screen.blit(button_0_text, button_0_text.get_rect(center=button_0.center))
+    pygame.draw.rect(screen, light_grey, button_1)
+    pygame.draw.rect(screen, grey, button_1, 1)
+    button_1_text = font.render("1", True, black)
+    screen.blit(button_1_text, button_1_text.get_rect(center=button_1.center))
+    pygame.draw.rect(screen, light_grey, button_2)
+    pygame.draw.rect(screen, grey, button_2, 1)
+    button_2_text = font.render("2", True, black)
+    screen.blit(button_2_text, button_2_text.get_rect(center=button_2.center))
+    pygame.draw.rect(screen, light_grey, button_3)
+    pygame.draw.rect(screen, grey, button_3, 1)
+    button_3_text = font.render("3", True, black)
+    screen.blit(button_3_text, button_3_text.get_rect(center=button_3.center))
+    pygame.draw.rect(screen, light_grey, button_4)
+    pygame.draw.rect(screen, grey, button_4, 1)
+    button_4_text = font.render("4", True, black)
+    screen.blit(button_4_text, button_4_text.get_rect(center=button_4.center))
+    pygame.draw.rect(screen, light_grey, button_5)
+    pygame.draw.rect(screen, grey, button_5, 1)
+    button_5_text = font.render("5", True, black)
+    screen.blit(button_5_text, button_5_text.get_rect(center=button_5.center))
+    pygame.draw.rect(screen, light_grey, button_6)
+    pygame.draw.rect(screen, grey, button_6, 1)
+    button_6_text = font.render("6", True, black)
+    screen.blit(button_6_text, button_6_text.get_rect(center=button_6.center))
+    pygame.draw.rect(screen, light_grey, button_7)
+    pygame.draw.rect(screen, grey, button_7, 1)
+    button_7_text = font.render("7", True, black)
+    screen.blit(button_7_text, button_7_text.get_rect(center=button_7.center))
+    pygame.draw.rect(screen, light_grey, button_8)
+    pygame.draw.rect(screen, grey, button_8, 1)
+    button_8_text = font.render("8", True, black)
+    screen.blit(button_8_text, button_8_text.get_rect(center=button_8.center))
+    pygame.draw.rect(screen, light_grey, button_9)
+    pygame.draw.rect(screen, grey, button_9, 1)
+    button_9_text = font.render("9", True, black)
+    screen.blit(button_9_text, button_9_text.get_rect(center=button_9.center))
+    pygame.draw.rect(screen, light_grey, button_dot)
+    pygame.draw.rect(screen, grey, button_dot, 1)
+    button_dot_text = font.render(".", True, black)
+    screen.blit(button_dot_text, button_dot_text.get_rect(center=button_dot.center))
 
+    pygame.display.flip()
 
-# Add buttons
-buttons = []  # Stores the tkinter buttons
-function_text = ["C", "/", "*", "-", "+", "=", "."]  # Allows for looping to add the buttons
-
-# Add function buttons to UI
-for i in range(7):
-    button = tk.Button(root, text=function_text[i], command=lambda x=function_text[i]: keypress(x))
-    button.grid(row=i < 4 and 1 or i == 4 and 2 or i - 1,
-                column=i == 6 and 2 or 6 > i > 3 and 3 or i,
-                rowspan=3 < i < 6 and 2 or 1,
-                sticky="nesw")
-    buttons.append(button)
-    # Symbol layout map
-    #   c/*-+=. sym
-    #   1111245 rows
-    #   0123332 cols
-    #   0123456 i
-
-# Add number buttons to UI
-for i in range(10):
-    button = tk.Button(root, text=f"{i}", command=lambda x=str(i): keypress(x))
-    if i == 0:
-        button.grid(row=5, column=0, columnspan=2, sticky="nesw")
-    if 1 <= i <= 3:
-        button.grid(row=4, column=i - 1, sticky="nesw")
-    if 4 <= i <= 6:
-        button.grid(row=3, column=i - 4, sticky="nesw")
-    if 7 <= i <= 9:
-        button.grid(row=2, column=i - 7, sticky="nesw")
-    buttons.append(button)
-
-# Fill window with buttons
-root.columnconfigure("all", weight=1)
-root.rowconfigure("all", weight=1)
-
-
-def update_display():
-    # Update the display to the current value
-    global right_value, display
-    display["text"] = str(right_value)
-
-
-def clear():
-    # Clear all values and call the display update
-    global left_value, right_value, current_result
-    left_value = str(0)
-    right_value = str(0)
-    current_result = str(0)
-    update_display()
-
-
-# Bind keys
-for i in range(10):
-    root.bind(str(i), lambda x: keypress(x.keysym))
-
-root.bind('<slash>', lambda x: keypress("/"))
-root.bind('<asterisk>', lambda x: keypress("*"))
-root.bind('<minus>', lambda x: keypress("-"))
-root.bind('<plus>', lambda x: keypress("+"))
-root.bind('<period>', lambda x: keypress("."))
-root.bind('<Return>', lambda x: keypress("="))
-root.bind('<Escape>', lambda x: keypress("C"))
-
-# Ensure focus for keybinds
-root.focus_set()
-
-# Run
-root.mainloop()
+pygame.quit()
